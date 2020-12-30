878ba5e63dce959839ca4a759cd44d64ebd81207
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-30 20:17:07 +0100

    render objects in context

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Question, Choice
 # Create your views here.
 def index(request):
-    context = {}
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
