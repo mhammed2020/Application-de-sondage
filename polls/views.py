c6aaa1e92167eda16d3b38f5c6d1d2ffcb863f5a
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-30 16:46:35 +0100

    create first views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Question, Choice
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'polls/index.html', context)
