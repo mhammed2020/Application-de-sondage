25fb028508bea970b94fdb98708791bf320643d3
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-30 16:18:14 +0100

    customize admin area

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+admin.site.site_header = "Sondage Admin"
+admin.site.site_title = "Sondage Admin zone"
+admin.site.index_title = "Bienvenue a l'administration"
+
+
 
 admin.site.register(Question)
 admin.site.register(Choice)
