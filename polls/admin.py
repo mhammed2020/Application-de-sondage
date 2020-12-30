44f5c376ae11a2581fbfbdd78e15e270a066a30d
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-30 16:39:23 +0100

    update django admin

--- polls/admin.py
+++ polls/admin.py
@@ -8,5 +8,14 @@ admin.site.index_title = "Bienvenue a l'administration"
 
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
